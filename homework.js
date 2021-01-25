afd7bbdeb7b8e797660032a9c600ac2f395d2e26
GrantDM <mullen.d.grant@gmail.com>
2021-01-25 12:20:45 -0500

    example 1 done

--- homework.js
+++ homework.js
@@ -1,4 +1,5 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = "Addison"
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
